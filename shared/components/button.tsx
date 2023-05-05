4fabf2ce70ff0400342336d70b7a9edf35419cfb
James Fitzgerald <james@jawfish.dev>
2023-05-05 11:57:26 -0400

    style: change button accent

--- shared/components/button.tsx
+++ shared/components/button.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default function Button({ className, href, children }: Props) {
   className = clsx(
-    'inline-flex items-center justify-center gap-2 rounded border-b-2 border-emerald-400 bg-zinc-800 py-2 px-3 text-sm font-semibold text-zinc-50 outline-offset-2 transition hover:bg-emerald-400 active:bg-zinc-800 active:text-zinc-100/70 active:transition-none dark:text-zinc-50 dark:active:bg-zinc-700 dark:active:text-zinc-100/70',
+    'inline-flex items-center justify-center gap-2 rounded bg-zinc-800 py-2 px-3 text-sm font-semibold text-zinc-50 outline-offset-2 transition hover:bg-emerald-400 active:bg-zinc-800 active:text-zinc-100/70 active:transition-none dark:text-zinc-50 dark:active:bg-zinc-700 dark:active:text-zinc-100/70',
     className
   );
 
